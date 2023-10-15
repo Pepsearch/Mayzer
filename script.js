2c47d26b7ea78f6fc15820794e5c00e231b31e8f
Prizmin <helloworldpy103@gmail.com>
2023-10-15 13:43:27 -0700

    Better UI

--- script.js
+++ script.js
@@ -21,24 +21,45 @@ function appendBotMessage(message) {
     chat.innerHTML += `<div class="bot-message">${message}</div>`;
 }
 
+function simulateTyping() {
+    chat.innerHTML += `<div class="bot-typing">Mayzer is typing</div>`;
+    chat.scrollTop = chat.scrollHeight;
+}
+
+function removeTypingIndicator() {
+    const typingIndicator = document.querySelector(".bot-typing");
+    if (typingIndicator) {
+        chat.removeChild(typingIndicator);
+    }
+}
+
 userInput.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
         const userMessage = userInput.value;
         appendUserMessage(userMessage);
         userInput.value = "";
 
-        // Convert user input to lowercase for case insensitivity
-        const userMessageLower = userMessage.toLowerCase();
+        // Simulate typing before bot response
+        simulateTyping();
 
-        // Iterate through keywords in a case-insensitive manner
-        let botResponse = botResponses["default"];
-        for (const keyword in botResponses) {
-            if (userMessageLower.includes(keyword)) {
-                botResponse = botResponses[keyword];
-                break;
+        // Simulate a slight delay in the bot's response
+        setTimeout(() => {
+            removeTypingIndicator();
+
+            // Convert user input to lowercase for case insensitivity
+            const userMessageLower = userMessage.toLowerCase();
+
+            // Iterate through keywords in a case-insensitive manner
+            let botResponse = botResponses["default"];
+            for (const keyword in botResponses) {
+                if (userMessageLower.includes(keyword)) {
+                    botResponse = botResponses[keyword];
+                    break;
+                }
             }
-        }
 
-        appendBotMessage(botResponse);
+            appendBotMessage(botResponse);
+            chat.scrollTop = chat.scrollHeight;
+        }, 1000); // Adjust the delay time as needed (1 second in this example)
     }
 });
